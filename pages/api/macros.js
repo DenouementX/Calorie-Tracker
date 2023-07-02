eab014d701e47a4fdc7d3a06d5098d6e8ff2a702
Lawrence <lmao7@jhu.edu>
2023-07-02 14:15:12 -0700

    api/findMany always returns ordered by index

--- pages/api/macros.js
+++ pages/api/macros.js
@@ -5,6 +5,11 @@ const prisma = new PrismaClient()
 export default async function handle(req, res) {
   var data = req.query;
   const macros = await prisma.macros.findMany({
+    orderBy: [
+      {
+        index: 'asc',
+      }
+    ],
     where: {
         AND: [
             {
